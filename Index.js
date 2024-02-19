bb93f500735f447ed5908f8d4092eeb8c30543a4
meinhoon <m4611780@gmail.com>
2024-02-19 01:34:35 +0100

    Required commit

--- Index.js
+++ Index.js
@@ -6,10 +6,10 @@ function menueToggle(){
 }
 
 var typed = new Typed(".typing", {
-    strings: ["Yotuber", "Progammer", "Vlogger","Dark Theme Person"
-                ,"C++", "Mirco-processor", "GhatalToPoland","RoboticsRevolution"],
-            typeSpeed: 100,
-            backSpeed: 60,
+    strings: ["Youtuber", "Progammer", "Vlogger","Dark Theme Person"
+                ,"C++", "Micro-processor", "GhatalToPoland","RoboticsRevolution"],
+            typeSpeed: 90,
+            backSpeed: 65,
             loop:true
 })
 
@@ -31,57 +31,4 @@ window.addEventListener('load', ()=>{
         const preload = document.querySelector('.load');
         preload.classList.add('load-fin');
         preload.style.display = "none";
-});
-
-// Designed by:  Mauricio Bucardo
-// Original image:
-// https://dribbble.com/shots/5619509-Animated-Tab-Bar
-
-"use strict"; 
-
-const body = document.body;
-const bgColorsBody = ["#ffb457", "#ff96bd", "#9999fb", "#ffe797", "#cffff1"];
-const menu = body.querySelector(".menu");
-const menuItems = menu.querySelectorAll(".menu__item");
-const menuBorder = menu.querySelector(".menu__border");
-let activeItem = menu.querySelector(".active");
-
-function clickItem(item, index) {
-
-    menu.style.removeProperty("--timeOut");
-    
-    if (activeItem == item) return;
-    
-    if (activeItem) {
-        activeItem.classList.remove("active");
-    }
-
-    
-    item.classList.add("active");
-    body.style.backgroundColor = bgColorsBody[index];
-    activeItem = item;
-    offsetMenuBorder(activeItem, menuBorder);
-    
-    
-}
-
-function offsetMenuBorder(element, menuBorder) {
-
-    const offsetActiveItem = element.getBoundingClientRect();
-    const left = Math.floor(offsetActiveItem.left - menu.offsetLeft - (menuBorder.offsetWidth  - offsetActiveItem.width) / 2) +  "px";
-    menuBorder.style.transform = `translate3d(${left}, 0 , 0)`;
-
-}
-
-offsetMenuBorder(activeItem, menuBorder);
-
-menuItems.forEach((item, index) => {
-
-    item.addEventListener("click", () => clickItem(item, index));
-    
-})
-
-window.addEventListener("resize", () => {
-    offsetMenuBorder(activeItem, menuBorder);
-    menu.style.setProperty("--timeOut", "none");
 });
